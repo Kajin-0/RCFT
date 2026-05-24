436ee66dc38742826f256dfcfbebf9c0bf5adaf5
Kajin-0 <28634576+Kajin-0@users.noreply.github.com>
2026-05-24 09:34:02 -0700

    Export certificate schema types

--- rcft/__init__.py
+++ rcft/__init__.py
@@ -1,11 +1,13 @@
 """RCFT modular-bootstrap research utilities."""
 
-from rcft.candidate_schema import Candidate, Character
+from rcft.candidate_schema import Candidate, CertificateStage, Character, GeneratorProvenance
 from rcft.validators import ValidationResult, validate_candidate
 
 __all__ = [
     "Candidate",
+    "CertificateStage",
     "Character",
+    "GeneratorProvenance",
     "ValidationResult",
     "validate_candidate",
 ]
